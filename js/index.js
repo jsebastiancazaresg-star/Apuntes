fcd169506b383c98b791a880d9ed4fd73486076f
sebastian <jsebastiancazaresg@gmail.com>
2025-10-06 18:42:22 -0500

    aprendi a usar html, css, JavaScript

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//alert("hola soy sebastian")
+let texto="sebastian"
+console.log(texto)
+let edad=12
+console.log(edad)
+console.log("hola soy "+ texto+ " y tengo "+edad)
+let amigos=["patricio", "alex","eduardo"]
+console.log(amigos)
+console.log(amigos[1])
+let animales=["perro", "gato", "pez"]
+let base=10
+let altura=15
+let resultado_triangulo=(base*altura)/2
+console.log (resultado_triangulo)
+console.log (`hola soy ${texto} i tengo ${edad} anos`)
