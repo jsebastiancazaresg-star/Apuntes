034571ff817663052aa03f5d7d5efed59f33b5b2
sebastian <jsebastiancazaresg@gmail.com>
2025-12-22 18:33:37 -0600

    apredni a poner imagenes y aprendi java scrpit

--- js/index.js
+++ js/index.js
@@ -20,4 +20,23 @@ let altura=15
 let resultado_triangulo=(base*altura)/2
 console.log (resultado_triangulo)
 console.log (`hola soy ${texto} i tengo ${edad} anos`)
+*/ 
+/*
+if (10>5) {
+    console.log ("10 es mayor que 5")
+} else{
+    console.log("es falso, 5 no es mayor que 10")
+}
 */
+let hora=""
+if (hora<=11 && hora>=5) {
+    console.log ("Buenos dias!")
+} else if(hora >=12 && hora <20){
+    console.log ("Buenas tardes!")
+}
+else if (hora>=20 && hora <=24 || hora >=0 && hora <5 ) {
+console.log ("Buenas noches!")
+}
+else {
+    console.log ("esa hora no es valida")
+}
